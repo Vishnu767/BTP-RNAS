20ae3c581550cd45b0f2a52daa37fc8c121943b3
vishnuvardhan <vishnuvardhanpuppala2003@gmail.com>
2024-03-17 23:49:47 +0530

    Changes in mras.py

--- NAO_V1/mras.py
+++ NAO_V1/mras.py
@@ -6,12 +6,15 @@ from torch.distributions.multivariate_normal import MultivariateNormal
 
 d = 2
 def H(arr,forward):
+    arr = torch.tensor([arr.tolist()]).cuda()
     encoder_outputs, encoder_hidden, arch_emb, predict_value = forward(arr)
+    print("Architecture inside passing to H(x): ", arr)
+    print("Predict Value: ", predict_value)
     return S(predict_value[0][0])
 
 def S(x):
     r = 1
-    return torch.exp(r*x)
+    return torch.exp(r*x).cuda()
 
 def H2(arr):
     d = len(arr)
@@ -24,16 +27,16 @@ def H2(arr):
 
 class pdf:
     def __init__(self):
-        self.mu = torch.zeros(d)
-        self.sigma = 10*torch.eye(d)
+        self.mu = torch.zeros(d).cuda()
+        self.sigma = 10*torch.eye(d).cuda()
         self.PDF = MultivariateNormal(self.mu, self.sigma)
 
     def f(self, x):
         return self.PDF.log_prob(x)
 
     def update(self, newMu, newSigma):
-        self.mu = newMu
-        self.sigma = newSigma
+        self.mu = newMu.cuda()
+        self.sigma = newSigma.cuda()
         for i in range(d):
           for j in range(d):
             if i!=j:
@@ -45,11 +48,13 @@ def I(x, gamma):
     return torch.where(x>=gamma, x, 0)
 
 def calculate_expection(X, func, k, pdf_function, gamma):
-    sum= 0
-    count =0
-    for x in X:
+    sum = func(X[0],k,pdf_function,gamma)
+    count = torch.tensor(1).cuda()
+    for i,x in enumerate(X):
+        if i==0:
+           continue
         sum += func(x, k, pdf_function, gamma)
-        count +=1
+        count += 1
     return sum / count
 
 def update_mu(X, gamma, k, pdf_function, forward):
@@ -58,40 +63,39 @@ def update_mu(X, gamma, k, pdf_function, forward):
       return (S(H(x,forward))**k) * I(H(x,forward),gamma) * x
     def func_denominator(x,k,pdf_function, gamma):
       return ((S(H(x,forward))**k) * I(H(x,forward),gamma))
-    return calculate_expection(X, func_numerator, k, pdf_function, gamma) / calculate_expection(X, func_denominator, k, pdf_function, gamma)
+    return torch.tensor(calculate_expection(X, func_numerator, k, pdf_function, gamma) / calculate_expection(X, func_denominator, k, pdf_function, gamma)).cuda()
 
 def update_sigma(X, gamma, k, pdf_function, forward):
     k = 1
     def func_numerator(x,k,pdf_function,gamma):
-      matrix1 = torch.unsqueeze(x-pdf_function.mu,dim=0)
-      matrix2 = torch.reshape(matrix1,(d,1))
+      matrix1 = torch.unsqueeze(x-pdf_function.mu,dim=0).cuda()
+      matrix2 = torch.reshape(matrix1,(d,1)).cuda()
       return (S(H(x,forward))**k) * I(H(x,forward),gamma) *(torch.matmul(matrix2,matrix1))
     def func_denominator(x,k,pdf_function,gamma):
       return ((S(H(x,forward))**k) * I(H(x,forward),gamma))
     arr = calculate_expection(X, func_numerator, k, pdf_function, gamma) / calculate_expection(X, func_denominator, k, pdf_function, gamma)
-    return arr
+    return torch.tensor(arr).cuda()
 
 def return_random_iids2(low, high, N):
-    randomIidsX = torch.tensor(np.random.uniform(low[0], high[0], N))
-    randomIidsY = torch.tensor(np.random.uniform(low[1], high[1], N))
+    randomIidsX = torch.tensor(np.random.uniform(low[0], high[0], N)).cuda()
+    randomIidsY = torch.tensor(np.random.uniform(low[1], high[1], N)).cuda()
     randomIids = [torch.tensor([randomIidsX[i].item(), randomIidsY[i].item()]) for i in range(N)]
-    return randomIids
+    return randomIids.cuda()
 
 def return_random_iids(N, prop_df):
     arr = []
     for i in range(N):
       arr.append(prop_df.PDF.sample().tolist())
-    return torch.tensor(arr)
+    return torch.tensor(arr).cuda()
     # return torch.tensor(np.random.multivariate_normal(pdf_function.mu.numpy(), pdf_function.sigma.numpy(), N))
 
-
-low = torch.tensor([-2, -2])
-high = torch.tensor([5, 5])
+low = torch.tensor([-2, -2]).cuda()
+high = torch.tensor([5, 5]).cuda()
 N = 100
 quantile = 0.08
 K = 5
-gamma = -1000
-epsilon = 0.001
+gamma = torch.tensor(-1000).cuda()
+epsilon = torch.tensor(0.001).cuda()
 alpha = 1.001
 
 def compare(X, Y):
@@ -108,7 +112,7 @@ def mras(arch,predict_lambda, forward):
     global d
     print("Number of architectures: ", len(arch))
     print("Length of each architecture: ", len(arch[0]))
-    randomIids = arch
+    randomIids = arch.cuda()
     alpha = predict_lambda
     N = len(arch)
     # Set the dimension too
@@ -121,6 +125,7 @@ def mras(arch,predict_lambda, forward):
         if randomIids == None:
             N = 100
             randomIids = return_random_iids(N, prop_df)
+        randomIids = randomIids.cuda()
         HValues = [H(i,forward) for i in randomIids]
         HValues_X = [[H(i,forward), i] for i in randomIids]
         sortedHValues = sorted(HValues)
@@ -139,4 +144,4 @@ def mras(arch,predict_lambda, forward):
         print(H(prop_df.mu, forward))
         randomIids = None
     
-    return return_random_iids(len(arch), prop_df)
+    return (return_random_iids(len(arch), prop_df)).cuda()
